772cf0bcbbbdf0fa868ae92fbe771c696cb19f8d
Abhay Kshatriya <kshatriya.abhay@iitg.ac.in>
2019-03-19 01:00:03 +0530

    zenfone3: dont use sgdisk,toybox in vendor assert
    * grep and echo are linked by recovery's busybox
    
    Signed-off-by: Abhay Kshatriya <kshatriya.abhay@iitg.ac.in>

--- device.mk
+++ device.mk
@@ -506,11 +506,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     android.hardware.thermal@1.0-impl
 
-# /vendor
-PRODUCT_PACKAGES += \
-   toybox_msm8953 \
-   sgdisk_msm8953
-
 # Checking /vendor
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/scripts/vendor.sh:install/bin/vendor.sh
--- scripts/vendor.sh
+++ scripts/vendor.sh
@@ -2,37 +2,17 @@
 
 # /sbin/sh runs out of TWRP.. use it.
 
-PROGS="sgdisk toybox"
-RC=0
-# Check for my needed programs
-for PROG in ${PROGS} ; do
-   if [ ! -x "/tmp/${PROG}" ] ; then
-      echo "Missing: /tmp/${PROG}"
-      RC=9
-   fi
-done
-#
-# all prebuilt
-if [ ${RC} -ne 0 ] ; then
-   echo "Aborting.."
-   exit 7
-fi
-TOYBOX="/tmp/toybox"
-
-# Get bootdevice.. don't assume /dev/block/sda
-# Dont remove 0 from mmcblk0p, assume system's number is two digit
-
-DISK=`${TOYBOX} readlink /dev/block/bootdevice/by-name/system | ${TOYBOX} sed -e's/p[0-9][0-9]//g'`
+# Get bootdevice.. don't assume /dev/block/mmcblk0p68
 
 # Check for /vendor existence
-VENDOR=`/tmp/sgdisk --pretend --print ${DISK} | ${TOYBOX} grep -c vendor`
+VENDOR=`ls -l /dev/block/bootdevice/by-name/ | grep -c vendor`
 
 if [ ${VENDOR} -ge 1 ] ; then
 # Got it, we're done...
-   ${TOYBOX} echo "/vendor partition found! "
+   echo "/vendor partition found! "
    exit 0
 fi
 
 # Missing...
-${TOYBOX} echo "/vendor partition missing! "
+echo "/vendor partition missing! "
 exit 1
